548c452eb3b3dafc8cc4a036134e0c3944b0fc69
fenya007 <59559796+fenya007@users.noreply.github.com>
2020-10-18 23:26:36 +0300

    add: two variables task

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ module.exports = function towelSort (matrix) {
   let res = [];
   if (!matrix) return [];
   for (let i = 0; i < matrix.length; i++){
-    if (i % 2 == 0) {
-      res.push(...matrix[i])
-    } else {
-      res.push(...matrix[i].reverse());
-    }
+  (i % 2 == 0) ? res.push(...matrix[i]) : res.push(...matrix[i].reverse());
   }
   return res
 }
